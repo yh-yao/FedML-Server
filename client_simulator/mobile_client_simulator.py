41f04c2d40f61b5f657f4b842b96b0e6b41ed61f
mzp0625 <mzma@usc.edu>
2020-12-08 17:57:14 -0800

    update to delete commented code

--- client_simulator/mobile_client_simulator.py
+++ client_simulator/mobile_client_simulator.py
@@ -232,11 +232,6 @@ if __name__ == '__main__':
     [train_data_num, test_data_num, train_data_global, test_data_global,
      train_data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num] = dataset
 
-    # load data
-    # dataset = load_data(args, args.dataset)
-    # [train_data_num, test_data_num, train_data_global, test_data_global,
-    #  train_data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num] = dataset
-
     # create model.
     # Note if the model is DNN (e.g., ResNet), the training will be very slow.
     # In this case, please use our FedML distributed version (./fedml_experiments/distributed_fedavg)
